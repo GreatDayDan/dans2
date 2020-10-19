fa794ebe7fb163580253dd9830bbde40f29f5e95
Dan Statham <Dan@GreatDayDan.com>
2020-10-19 13:24:19 -0500

    trying to change the description

--- storage/framework/views/278303a3f841f8a765bcb4bfa95fe05a8367aff3.php
+++ storage/framework/views/278303a3f841f8a765bcb4bfa95fe05a8367aff3.php
@@ -17,8 +17,13 @@
 
 
         <!-- Scripts -->
-        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.6.0/dist/alpine.js" defer></script>
-        <script src="E:\wamp64\www\dans2\public\js\ChangeDescription.js"></script>
+        <script  type="text/javascript"   src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.6.0/dist/alpine.js" defer></script>
+        <script  type="text/javascript"   src="..\..\public\js\ChangeDescription.js"></script>
+
+
+        
+
+
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
--- storage/framework/views/67168ea5afac09248a56d8f9b01f76883016fe6a.php
+++ storage/framework/views/67168ea5afac09248a56d8f9b01f76883016fe6a.php
@@ -6,26 +6,27 @@
 
         </div>
     <?php endif; ?>
-      <form name='f1' action="/addEvent1"  method="put">
+      <form name='f1' action="<?php echo e(url('/addEvent')); ?>"  method="post">
           <?php echo csrf_field(); ?>
           <div class="form-group">
               <label for="event_id">Choose an Event</label>
-              <select name="event_id" id="event_id" class="form-control" required onChange="ChangeDescription.js($this=>'id')">
+              <select name="event_id" id="event_id" size="" class="form-control">
                   <?php $__currentLoopData = $jdevents; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $event): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
                       <option name="pid" value="<?php echo e($event->id); ?>" id="event"><?php echo e($event->event); ?></option>
                   <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
               </select>
               <p>
                   <input type="text" id="hideme">
-                  <input type="text" id="ename" name="EVENT" width="600" value =""
+                  <input type="text" id="ename" name="EVENT" width="600" value=""
                          placeholder="Select an existing event from the list or enter a new event here.">
               </p>
               <p>
                   <label id="label4" for="DESCRIPTION">Description</label>
-                  <textarea id ="descr" rows="5" cols="50" form="f1" name="DESCRIPTION" placeholder="Describe the event" id="DESCRIPTION"></textarea>
+                  <textarea id="descr" rows="5" cols="50" form="f1" name="DESCRIPTION" placeholder="Describe the event"
+                            id="DESCRIPTION"></textarea>
               </p>
 
-              <input type='submit' id='submit_id' name='submit_id' >
+              <input type='submit' id='submit_id' name='submit_id'>
           </div>
     </form>
   </div>
